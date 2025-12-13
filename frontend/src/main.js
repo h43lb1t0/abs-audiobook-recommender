52559450da0d3e609e81eefc7dae58dcfc3e0eaa
Tom Haelbich <65122811+h43lb1t0@users.noreply.github.com>
2025-12-13 03:14:12 +0100

    fixed frontend not telling backend about language

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,4 +5,18 @@ import router from './router'
 
 import i18n from './i18n'
 
+import axios from 'axios'
+import { watch } from 'vue'
+
+// Set initial header
+axios.defaults.headers.common['Accept-Language'] = i18n.global.locale.value
+
+// Watch for language changes
+watch(
+    () => i18n.global.locale.value,
+    (newLocale) => {
+        axios.defaults.headers.common['Accept-Language'] = newLocale
+    }
+)
+
 createApp(App).use(router).use(i18n).mount('#app')
